5b6681442f35c62966585def35502a92cc567c4a
Ivan Shagun <136569386+IvanShagun@users.noreply.github.com>
2023-07-02 18:43:12 +0400

    Add files via upload

---
+++ DZ6.py
@@ -0,0 +1,23 @@
+# a = int(input("Введите первый элемент: "))
+# d = int(input("Введите шаг: "))
+# n = int(input("Введите количество элементов: "))
+# mas = list()
+# for i in range(n):
+#   mas.append(str(a + i * d)) 
+# print(*mas)
+
+
+
+# import random
+# mas = [random.randint(-10, 10) for i in range(int(input('Количество элементов: ')))]
+# print(mas)
+# a = int(input('От: '))
+# b = int(input('До: '))
+# if a > b:
+#     print('Неверный диапазон')
+# else:
+#     for i, v in enumerate(mas, start=0):
+#         if v >= a and v <= b:
+#             print(i, end=' ')
+
+
